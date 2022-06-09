200f11dc571fb1b24eccb863849a1fc90205b0d1
Antonio Carlos Flach <flach_br@hotmail.com>
2022-06-09 00:17:40 -0300

    List tasks

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -3,8 +3,10 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Task from 'App/Models/Task'
 
 export default class TasksController {
-  public index({ view }: HttpContextContract) {
-    return view.render('tasks/index')
+  public async index({ view }: HttpContextContract) {
+    const tasks = await Task.all()
+
+    return view.render('tasks/index', { tasks })
   }
 
   public async store({ request, response, session }: HttpContextContract) {
