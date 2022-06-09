60457c234598ddc6e853e7ddad434f6008e00603
Antonio Carlos Flach <flach_br@hotmail.com>
2022-06-08 22:35:21 -0300

    first commit

---
+++ app/Controllers/Http/TasksController.ts
@@ -0,0 +1,31 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import Task from 'App/Models/Task'
+
+export default class TasksController {
+  public index({ view }: HttpContextContract) {
+    return view.render('tasks/index')
+  }
+
+  public async store({ request, response, session }: HttpContextContract) {
+    const validationSchema = schema.create({
+      title: schema.string({ trim: true }, [rules.maxLength(255)]),
+    })
+
+    const validatedData = await request.validate({
+      schema: validationSchema,
+      messages: {
+        'title.required': 'Enter tasks title',
+        'title.maxLength': 'Task title can not exceed 255 characters',
+      },
+    })
+
+    await Task.create({
+      title: validatedData.title,
+    })
+
+    session.flash('notification', 'Task added!')
+
+    return response.redirect('back')
+  }
+}
